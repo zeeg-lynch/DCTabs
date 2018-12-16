bc02826acca553d610a3ba46913695b77ed90f83
zeeg-lynch <zeeglynch@gmail.com>
2018-12-17 01:07:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+new Vue({
+	el: '#app',
+	data: {
+		title: "HW",
+		selectedLink: "",
+		newTabName: "",
+		newTabLink: "",
+		tabs: [
+			{name: "Take A Look Around", link: "https://tabs.ultimate-guitar.com/tab/limp_bizkit/take_a_look_around_tabs_9751"},
+			{name: "The Unforgiven", link: "https://tabs.ultimate-guitar.com/tab/metallica/the_unforgiven_tabs_74957"}
+		],
+		exportedTabs: "",
+		checked_id: "",
+		addActive: false,
+	},
+	mounted() {
+	    if (localStorage.tabs) {
+	      try {
+	        this.tabs = JSON.parse(localStorage.getItem('tabs'));
+	      } catch(e) {
+	        localStorage.removeItem('tabs');
+	      }
+	    }
+    },
+	watch: {
+	    tabs(newTabs) {
+	    	console.log("watch triggered");
+	        localStorage.tabs = JSON.stringify(newTabs);
+	    }
+	},
+	computed: {
+		exportedTabsIsNull: function() {
+			return this.exportedTabs==="";
+		}
+	},
+	methods: {
+		updateSelectedLink(link) {
+			console.log("updateSelectedLink invoked. Link is " + link);
+			this.selectedLink = link;
+			console.log("selectedLink is " + this.selectedLink);
+		},
+
+		addTab: function() {			
+			if (this.newTabName!="" && this.newTabLink!="") {
+				this.tabs.push({name:this.newTabName,link:this.newTabLink});
+				this.newTabName="";
+				this.newTabLink="";
+			}	
+			this.addActive=!this.addActive;			
+			// this.saveToLocalStorage();
+		},
+
+		exportTabs: function() {
+			console.log("EXPORT: " + this.exportedTabs);
+			if (this.exportedTabs==="") {
+				this.exportedTabs = JSON.stringify(this.tabs);				
+			}
+			else {
+				this.exportedTabs="";
+			}
+		},
+
+		setChecked: function(e) {
+			var checkedId = e.target.id;
+			console.log(checkedId);
+			if (this.checked_id != checkedId) {
+				this.checked_id = checkedId;				
+			}
+			else {
+				this.checked_id = '';
+			}
+		},
+
+		removeTab: function(tab) {
+			this.tabs.splice(this.tabs.indexOf(tab));
+		}
+
+
+	}
+});
